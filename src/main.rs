217411a2b5b6e88b1832e8e046fe94087737f1ec
João Lucas <joaolucasfp2001@gmail.com>
2025-12-18 00:58:36 -0300

    feat: create base command (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+use heroesofcode_git::repos::Repos;
+
+#[derive(Parser)]
+struct Args {
+	/// Optional subcommand that defines an alternative executions flow
+	#[command[subcommand]]
+	command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+	/// List all repositories
+	Repos,
+	/// Clone some project
+	Clone,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+	let args = Args::parse();
+
+	match args.command {
+		Some(Command::Repos) => {
+			Repos::response().await?;
+		}
+		Some(Command::Clone) => {
+			println!("Clone some project");
+		}
+		None => {
+			println!("Run --help");
+		}
+	}
+
+	Ok(())
 }
