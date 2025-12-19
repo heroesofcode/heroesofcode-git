aa6fc375f1b1044ec132b618cadf2909e4e5423b
João Lucas <joaolucasfp2001@gmail.com>
2025-12-19 12:08:54 -0300

    refactor: separate initializer the tool in another struct (#9)

---
+++ src/cli.rs
@@ -0,0 +1,42 @@
+use clap::{Parser, Subcommand};
+
+use crate::clone::Clone;
+use crate::repos::Repos;
+
+#[derive(Parser)]
+struct Args {
+	/// Optional subcommand that defines an alternative executions flow
+	#[command[subcommand]]
+	command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+	/// List all repositories
+	Repos,
+	/// Clone some project
+	Clone,
+}
+
+pub struct Cli;
+
+impl Cli {
+	/// Function to initialize the tool
+	pub async fn start() -> Result<(), reqwest::Error> {
+		let args = Args::parse();
+
+		match args.command {
+			Some(Command::Repos) => {
+				Repos::list_all().await?;
+			}
+			Some(Command::Clone) => {
+				Clone::clone_repos().await?;
+			}
+			None => {
+				println!("Run --help");
+			}
+		}
+
+		Ok(())
+	}
+}
